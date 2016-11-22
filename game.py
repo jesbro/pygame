54878c75507292c7d2aefc60629374bf97137a01
jesbro <jesbro@umich.edu>
2016-11-21 21:21:10 -0500

    updting game.py and adding pic for testing

---
+++ game.py
@@ -0,0 +1,34 @@
+'''
+Created by Jess Brown, 2016
+jesbro@umich.edu
+SI 206, Project 4: Pygame
+'''
+
+from pygame import *
+import random
+from pygame.sprite import *
+
+pygame.init();
+
+DELAY = 8000;
+bgcolor = (0,0,255)
+
+class Something(Sprite):
+	def __init__(self):
+		Sprite.__init__(self)
+		self.image = image.load("loco.jpg").convert_alpha()#or loco.bmp
+		self.rect = self.image.get_rect()
+
+#def hit(self, target)
+	#return self.rect.colliderect(target)
+init()
+
+screen = display.set_mode((640, 480))
+display.set_caption("Games are fun")
+
+
+sp = Something()
+sprites = RenderPlain(sp)
+
+#time.set_timer(USEREVENT+ 1, DELAY) is for timer
+
