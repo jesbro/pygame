038f2eaf54f3bb254cea816e01a4ea3e0c38b703
jesbro <jesbro@umich.edu>
2016-12-02 16:32:52 -0500

    final commit (hopefully)

--- game.py
+++ game.py
@@ -2,12 +2,23 @@
 Created by Jess Brown, 2016
 jesbro@umich.edu
 SI 206, Project 4: Pygame
+Bloon Popper
 '''
 
 '''
 References:
-https://books.google.com/books?id=RLU1Gkm1JGoC&pg=PA442&lpg=PA442&dq=pygame+wait+to+start+game&source=bl&ots=pkJHMtgvAC&sig=DHPSwufY7L2XPQoGtY5rYwHM0p8&hl=en&sa=X&ved=0ahUKEwjb3tTZ0czQAhUJbSYKHVGjA_oQ6AEIQzAG#v=onepage&q=pygame%20wait%20to%20start%20game&f=false
-http://nullege.com/codes/show/src@g@a@GameDevelopment-HEAD@examples@students_works@pygame@farmyard@FarmyardRoundEmUp.py/861/pygame.event.wait
+
+Idea taken from Bloons Tower Defense by Ninja Kiwi
+
+https://books.google.com/books?id=RLU1Gkm1JGoC&pg=PA442&lpg=PA442&dq=
+    pygame+wait+to+start+game&source=bl&ots=pkJHMtgvAC&sig=DHPSwufY7L2X
+    PQoGtY5rYwHM0p8&hl=en&sa=X&ved=0ahUKEwjb3tTZ0czQAhUJbSYKHVGjA_oQ6AE
+    IQzAG#v=onepage&q=pygame%20wait%20to%20start%20game&f=false
+
+http://nullege.com/codes/show/src@g@a@GameDevelopment-HEAD@examples@stu
+    dents_works@pygame@farmyard@FarmyardRoundEmUp.py/861/pygame.event.wait
+
+http://www.pygame.org/docs/ref/mixer.html
 '''
 
 from pygame import *
@@ -16,11 +27,14 @@ from pygame.sprite import *
 
 pygame.init();
 
-DELAY = 15;
+DELAY = 18;
 blue = (50, 150, 255)
-green = (122, 244, 66)
+green = (151, 188, 56)
 black = (0,0,0)
 white = (255,255,255)
+brown = (206, 151, 97)
+gray = (188, 188, 188)
+red = (255, 0, 0)
 
 screenx = 900 #leaving room around the edges
 screeny = 600
@@ -29,22 +43,32 @@ bloons = pygame.sprite.Group()
 stars = pygame.sprite.Group()
 monkeys = pygame.sprite.Group()
 
+bgmusic = mixer.Sound(r"media\music.wav")
+bgmusic.set_volume(.9)
+spop = mixer.Sound(r"media\pop.wav")
+softpop = mixer.Sound(r"media\pop.wav")
+softpop.set_volume(.15)
+
 class Border(Sprite):
 
-    def __init__(self):
+    def __init__(self, mid=False):
         Sprite.__init__(self)
 
         im = image.load(r"media\path_large.png").convert_alpha()
-        self.image = pygame.transform.scale(im, (900, 100))
-        
-        # self.image = pygame.Surface([900, 100], pygame.SRCALPHA, 32)
-        # self.image.fill((255,0,255,50))
+
+        if mid == False:
+            self.image = pygame.transform.scale(im, (900, 100))
+        else:
+            self.image = pygame.transform.scale(im, (900, 210))
 
         self.rect = self.image.get_rect()
 
     def setpos(self, x, y):
         self.rect.x = x
         self.rect.y = y
+
+    def hit(self, monk):
+        return self.rect.colliderect(monk)
 
 class Monkey(Sprite):
 
@@ -108,12 +132,8 @@ class Bloon(Sprite):
             self.image = pygame.transform.scale(im, (70, 90))
             self.speed = 2
 
-        elif color == "rb":
-            im = image.load(r"media\bloon_red.png").convert_alpha()
-            self.image = pygame.transform.scale(im, (60, 80))
-            self.speed = 2
-
         self.rect = self.image.get_rect()
+        self.defense = len(monkeys)
 
     def hit(self, border):
         return self.rect.colliderect(border)
@@ -126,32 +146,39 @@ class Bloon(Sprite):
         self.kill()
 
     def travel(self, ud):
-
         if ud:
-            self.rect.y += self.speed
+            self.rect.y += 1
         else:
-            self.rect.y -= self.speed
+            self.rect.y -= 1
         self.rect.x += self.speed
         
     def escaped(self):
         if self.rect.center > (screenx, screeny): 
             return True
         return False
-        
 
+    def getdef(self):
+        return self.defense
+
+    def lowerd(self):
+        self.defense -= 1
+        
 def stop(gameover=False, pause=False, begin=False):
     t3 = fbig.render(" Game Over ", False, black, white)
     t4 = f.render(" Click anywhere to continue ", False, black, white)
     t5 = fbig.render(" Paused ", False, black, white)
-    t6 = fmed.render(" Click on the grass to place a monkey! ", False, blue)
-    t7 = f.render(" Monkeys on the top shoot down and right ", False, white, black)
-    t8 = f.render(" Monkeys on the bottom shoot up and left ", False, white, black)
+    t6 = fmed.render(" Click on the grass to place a monkey! ", False, blue, black)
+    #t7 = f.render(" Monkeys on the top shoot down and right ", False, white, black)
+    #t8 = f.render(" Monkeys on the bottom shoot up and left ", False, white, black)
+    t9 = f.render(" Press any key to shoot a ninja star ", False, green, black)
+    t10 = f.render(" Click anywhere to exit ", False, black, white)
+    t11 = t = f.render(" Place monkey on the grass ", False, black, red)
 
     if gameover:
         while True:
             e = pygame.event.wait()
-            screen.blit(t3, (275, 220))
-            screen.blit(t4, (330, 288))
+            screen.blit(t3, (220, 220))
+            screen.blit(t10, (315, 315))
 
             if e.type in (pygame.QUIT, pygame.MOUSEBUTTONDOWN): return
 
@@ -160,8 +187,8 @@ def stop(gameover=False, pause=False, begin=False):
     elif pause: 
         while True:
             e = pygame.event.wait()
-            screen.blit(t5, (325, 220))
-            screen.blit(t4, (329, 288))
+            screen.blit(t5, (275, 220))
+            screen.blit(t4, (270, 310))
 
             if e.type in (pygame.QUIT, pygame.MOUSEBUTTONDOWN): return
 
@@ -170,23 +197,37 @@ def stop(gameover=False, pause=False, begin=False):
     elif begin:
         while True:
             e = pygame.event.wait()
-            screen.blit(t6, (80, 220))
-            screen.blit(t7, (250, 100))
-            screen.blit(t8, (250, 500))
 
-            if e.type in (pygame.QUIT, pygame.MOUSEBUTTONDOWN): return
+            screen.blit(t6, (10, 220))
+            #screen.blit(t7, (200, 100))
+            #screen.blit(t8, (200, 500))
+            screen.blit(t9, (240, 390))
+
+            if e.type in (pygame.QUIT, pygame.MOUSEBUTTONDOWN):
+                x, y = mouse.get_pos()
+                if y > 300 and y < 475:
+                    screen.blit(t11, (200,150))
+                else:
+                    return
 
             display.update()
 
 def addMonkey():
 
-    t = fmed.render("You earned another monkey! Click to place", False, black)
+    t = fms.render("You earned another monkey! Click to place", False, black, green)
+    t11 = f.render(" Place monkey on the grass ", False, black, red)
 
     while True:
         e = pygame.event.wait()
-        screen.blit(t, (15, 220))
+        screen.blit(t, (5, 260))
         
-        if e.type in (pygame.QUIT, pygame.MOUSEBUTTONDOWN): return
+        if e.type in (pygame.QUIT, pygame.MOUSEBUTTONDOWN): 
+            x, y = mouse.get_pos()
+            if y > 300 and y < 475:
+                screen.blit(t11, (200,150))
+            else:
+                return
+
         display.update()
 
 #determine bloon/star collision, returns num of hits
@@ -195,14 +236,20 @@ def collide():
     temp = 0
     for b in bloons:
         for s in stars:
-            if b.hit(s):
-
-                mixer.Sound(r"media\pop.wav").play()
+            if b.hit(s) and b.getdef() == 1:
+                spop.play()
                 stars.remove(s)
                 s.kill()
                 bloons.remove(b)
                 b.pop()
                 temp += 1
+            elif b.hit(s):
+                softpop.play()
+                b.lowerd()
+                stars.remove(s)
+                s.kill()
+                break
+
     return temp
 
 def escaped():
@@ -211,8 +258,8 @@ def escaped():
     for b in bloons:
         if b.escaped():
             temp += 1
-            bloons.remove(bl)
-            bl.pop()
+            bloons.remove(b)
+            b.pop()
     return temp
 
 def throw():
@@ -222,28 +269,32 @@ def throw():
         else: star = BottomStar()
         star.setpos(x+20, y)
 
-def runbloons(times=1):
-    for x in range(times):
-        if numbloons < 8:
-            newbloon = Bloon("red")
-            newbloon.setpos(-50, 328)
+def runbloons():
+    temp = 0
+    if numbloons < 8:
+        newbloon = Bloon("red")
+        newbloon.setpos(-50, 328)
+        temp += 1
 
-        elif numbloons < 14:
-            nb = Bloon("rb")
-            newbloon = Bloon("blue")
-            nb.setpos(-50, 328)
-            newbloon.setpos(-50, 328)
+    elif numbloons < 20:
+        newbloon = Bloon("blue")
+        newbloon.setpos(-50, 328)
+        temp += 1
 
+    return temp
 
 #main
 init()
 
+bgmusic.play()
 screen = display.set_mode((900, 600))
 display.set_caption("Pop the bloons!")
 
-f = font.Font(None, 30)
-fmed = font.Font(None, 60)
-fbig = font.Font(None, 100)
+f = font.SysFont("candara", 30)
+fmed = font.SysFont("candara", 55)
+fms = font.SysFont("candara", 49)
+fsmall = font.SysFont("candara", 20)
+fbig = font.SysFont("candara", 100)
 
 b1 = Border() #top border
 b2 = Border() #bottom border
@@ -258,19 +309,18 @@ bg = image.load(r"media\path_bg.png").convert_alpha()
 screen.blit(bg, (0,0)) #sets background image
 
 hits = 0 #bloons hit
-health = 10 #health -= 1 for every escaped bloon
+health = 20 #health -= 1 for every escaped bloon
 temphits = 0 #keep track of when to add monkeys
-
 udtrigger = True #true = need to go up, false = need to go down
+numbloons = 0
+loopcounter = 0
+
 stop(begin=True)
 
-#set monkey position
 x, y = mouse.get_pos()
 monkey.setpos(x, y)
 
 sprites = RenderPlain(monkeys)
-loopcounter = 0
-numbloons = 0
 
 #game loop
 while True:
@@ -298,38 +348,40 @@ while True:
         throw()        
 
 #if there is a hit, kill the bloon (and star)
-    c = collide()
-    hits += c
-    temphits += c
+    hits += collide()
 
 #if bloon escaped, subtract health. check if health = 0, if true game is over
     health -= escaped()
-    if health <= 0: stop(gameover=True)
+    if health <= 0: 
+        stop(gameover=True)
+        break
 
 #sends bloons
-    if loopcounter % 200 == 0:
-        runbloons(len(monkeys))
-        numbloons += 1
+    if loopcounter % 100 == 0:
+        numbloons += runbloons()
+        
     loopcounter += 1     
 
-    if len(bloons) == 0 and temphits >= 10: 
+    if len(bloons) == 0 and numbloons >= 20: 
         addMonkey()
         x, y = mouse.get_pos()
         m = Monkey()
         m.setpos(x, y)
-        temphits = 0
         numbloons = 0
-
+        temphits = 0
 
     if len(stars) > 0: sprites = RenderPlain(bloons, monkeys, stars)
     else: sprites = RenderPlain(bloons, monkeys)
 
 #render and update screen, sprites, and text
+    
     t = f.render("Hits: " + str(hits), False, black)
+    t1 = fsmall.render("Click to pause", False, black)
     t2 = f.render("Health: "+ str(health), False, black)
     screen.blit(bg, [0,0])
-    screen.blit(t, (700, 20)) 
-    screen.blit(t2, (700, 50))  
+    screen.blit(t, (750, 20)) 
+    screen.blit(t1, (5, 575))
+    screen.blit(t2, (750, 50))  
     sprites.update()
     sprites.draw(screen)
     display.update()
